d2ebd607528baa503ab5879b45834fab8538406a
Aditya pandey <honeypandey409@gmail.com>
2025-03-02 21:04:30 +0530

    Create function.sumofn.cpp

---
+++ function.sumofn.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int sumofn(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        return sum+=i;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<< sumofn(n);
+}

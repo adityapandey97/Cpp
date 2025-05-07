1e9da5a18dca2faf24bb2345f1d76f77583ad07f
Aditya pandey <honeypandey409@gmail.com>
2025-05-07 23:34:53 +0530

    Create BS.Fristoccarance.cpp

---
+++ BS.Fristoccarance.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int fristoccurance(int arr[],int n,int target){
+    int start=0;
+    int end=n-1;
+    int mid=(start+end)/2;
+    int ans=0;
+    while(start<=end){
+        if(arr[mid]==target){
+            ans=mid;
+            end=end-1;
+        }else if(arr[mid]<target){
+            start=mid+1;
+        }else if(arr[mid]>target){
+            end=mid-1;
+        }
+        mid=(start+end)/2;
+    }
+    return ans;
+
+}
+int main(){
+    int arr[]={10,20,30,30,30,40,50,60,70};
+    int n=9;
+    int target=30;
+   int result= fristoccurance(arr,n,target);
+   if(result==-1){
+    cout<<"target was not found";
+   }else{
+    cout<<"target was found at indux:"<<result;
+   }
+}
